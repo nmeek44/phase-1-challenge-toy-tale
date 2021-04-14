9048aa7da2ed5b0446566624d42cc02e2ec3819b
nmeek44 <nmeek44@gmail.com>
2021-04-14 11:48:47 -0400

    Part 1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -12,4 +11,66 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  let toySubmit = document.querySelector(".add-toy-form")
+  toySubmit.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let 
+    console.log(e.target[0])
+    debugger
+  })
 });
+
+fetch("http://localhost:3000/toys")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(obj) {
+    loadToys(obj)
+  })
+
+function loadToys(obj) {
+  obj.forEach(function (toy) {
+    let cardDiv = document.createElement("div")
+    cardDiv.class = "card"
+
+    let toyTag = document.createElement("h2")
+    toyTag.innerText = toy.name
+    cardDiv.append(toyTag)
+    
+    let toyImg = document.createElement("img")
+    toyImg.src = toy.image
+    toyImg.class = "toy-avatar"
+    cardDiv.append(toyImg)
+    
+    let toyP = document.createElement("p")
+    toyP.innerText = (`${toy.likes} Likes`)
+    cardDiv.append(toyP)
+
+    let toyButton = document.createElement("button")
+    toyButton.class = "like-btn"
+    toyButton.id = toy.id
+    
+    let toyLocation = document.querySelector("#toy-collection")
+    cardDiv.append(toyTag, toyImg, toyP, toyButton)
+    toyLocation.append(cardDiv)
+
+  })
+}
+  
+//   let postObj = {
+//     method:"POST"
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     }
+//     body: JSON.stringify({
+//       "name": ""
+//       "image": ""
+//       "likes": 
+//     })
+//   }
+
+//   fetch("http://localhost:3000/toys", configObj)
+
+
+// }
